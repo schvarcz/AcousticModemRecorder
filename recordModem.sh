41783cdd082ac40cf97f60fdd0ddefdb808bc391
Guilherme Schvarcz Franco <guilhermefrancosi@gmail.com>
2014-09-05 15:02:10 +0200

    Just adds the bps settings for the serial ports

--- recordModem.sh
+++ recordModem.sh
@@ -2,6 +2,10 @@
 
 mkdir datasets &> /dev/null
 
+#Not tested yet, but should set the right bps for the modems
+stty -F /dev/ttyUSB0 speed 9600
+stty -F /dev/ttyUSB1 speed 9600
+
 echo "./pinger.sh  
 ./output.sh  < /dev/ttyUSB1 > datasets/master
  ./output.sh < /dev/ttyUSB0 > datasets/slave
